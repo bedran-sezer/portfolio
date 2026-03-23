6a1cee26ad3f6668b401c837f2dcd46c192a4fb1
bedran-sezer <bedi10@hotmail.fr>
2026-03-23 19:20:57 +0100

    feat: add skills section with dynamic data and ngFor

--- src/app/features/skills/skills.component.ts
+++ src/app/features/skills/skills.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule, NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
+  categories = [
+    {
+      name: 'Frontend',
+      skills: ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'Angular']
+    },
+    {
+      name: 'Backend',
+      skills: ['Node.js / Express', 'PHP', 'API REST']
+    },
+    {
+      name: 'Base de données',
+      skills: ['MySQL']
+    },
+    {
+      name: 'Outils',
+      skills: ['Git', 'Docker', 'Linux', 'Windows', 'VSCode']
+    }
+  ];
 
 }
