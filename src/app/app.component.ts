60838158ab28f89bc4819b792189c2a4ea557907
bedran-sezer <bedi10@hotmail.fr>
2026-03-23 18:32:25 +0100

    feat: add about section HTML structure with personal info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { HeroComponent } from './features/hero/hero.component';
+import { AboutComponent } from './features/about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent,HeroComponent],
+  imports: [RouterOutlet, NavbarComponent,HeroComponent,AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
