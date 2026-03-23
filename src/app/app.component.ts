a6250f91b4abd343b3b829c9b83ff555afdd92c7
bedran-sezer <bedi10@hotmail.fr>
2026-03-23 19:03:37 +0100

    feat: integrate all feature components into app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { HeroComponent } from './features/hero/hero.component';
 import { AboutComponent } from './features/about/about.component';
+import{ ContactComponent } from './features/contact/contact.component';
+import { ProjectsComponent } from './features/projects/projects.component';
+import { SkillsComponent } from './features/skills/skills.component';
+import { EducationComponent } from './features/education/education.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent,HeroComponent,AboutComponent],
+  imports: [RouterOutlet, NavbarComponent,HeroComponent,AboutComponent,ContactComponent,ProjectsComponent,SkillsComponent,EducationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
