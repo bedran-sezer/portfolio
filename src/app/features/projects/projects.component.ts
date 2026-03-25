7fcd9dbada74356c26e8c78721851e23a1cb41a8
bedran-sezer <bedi10@hotmail.fr>
2026-03-25 20:21:07 +0100

    feat: add projects section with dynamic data and ngFor

--- src/app/features/projects/projects.component.ts
+++ src/app/features/projects/projects.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
-
+  projects = [
+    {
+      title: 'Dojo',
+      description: 'Projet de diplôme Bachelor à l\'HEPIA/HES-SO de Genève. Création d\'une interface web et amélioration d\'un CLI existant. Plateforme d\'échange d\'exercices et énoncés entre professeurs et étudiants, avec une étude sur la gamification de la plateforme.',
+      techs: ['Angular', 'TypeScript', 'MySQL', 'PHPMyAdmin', 'Postman', 'Figma'],
+      github: '',
+      image : '',
+      live: '',
+      type: 'Projet de diplôme'
+    },
+    {
+      title: 'Ekmek Teknesi',
+      description: 'Site vitrine pour le commerce de famille. Projet personnel pour mettre en avant les produits et services de la boulangerie.',
+      techs: ['HTML', 'CSS', 'JavaScript'],
+      github: '',
+      image : '',
+      live: '',
+      type: 'Projet personnel'
+    },
+    {
+      title: 'IB Plâtrerie',
+      description: 'Site vitrine pour une société de plâtrerie. Projet réalisé pour prendre en main WordPress et Elementor.',
+      techs: ['WordPress', 'Elementor'],
+      github: '',
+      image : '',
+      live: '',
+      type: 'Projet freelance'
+    }
+  ];
 }
